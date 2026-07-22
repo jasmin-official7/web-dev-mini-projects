17269ec60c2aecade9d872b6a956f36fd4b3a931
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2026-07-22 21:45:57 +0530

    fix:merge conflicts

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let score=JSON.parse(localStorage.getItem('score')) || {
     wins:0,
     loses:0,
@@ -74,81 +73,4 @@ function pickComputerMove(){ //WHAT COMPUTER CHOOSES
   }
   
   return compMove;  
-=======
-let score=JSON.parse(localStorage.getItem('score')) || {
-    wins:0,
-    loses:0,
-    ties:0
-  }; 
-  updateScore();
-
-function playGame(playerMove){ //WHAT USER CHOOSES
-  
-  const compMove = pickComputerMove(); 
-  let result='';
-  
-  if(playerMove==='Scissors'){
-    if(compMove==='Rock'){
-      result='You Lose.';
-    }else if(compMove==='Paper'){
-      result='You Win.';
-    }else{
-      result='Tie.';
-    } 
-  }else if(playerMove==='Paper'){
-    if(compMove==='Rock'){
-    result='You Win.';
-    }else if(compMove==='Paper'){
-      result='Tie.';
-    }else{
-      result='You Lose.';
-    }
-  }else{
-    if(compMove==='Rock'){
-    result='Tie.';
-    }else if(compMove==='Paper'){
-      result='You Lose.';
-    }else{
-      result='You Win.';
-    }
-  }
-
-  if(result==='You Win.'){
-    score.wins+=1 ; 
-  }else if(result==='You Lose.'){
-    score.loses+=1;
-  }else if(result==='Tie.'){
-    score.ties+=1;
-  }
-
-
-  localStorage.setItem('score',JSON.stringify(score));
-
-  updateScore();
-  document.querySelector('.js-result').innerHTML= result;
-  document.querySelector('.js-moves').innerHTML=`You
-  <img src="images/${playerMove}-emoji.png" class="move-icon">
-  <img src="images/${compMove}-emoji.png" class="move-icon">
-  Computer`;
-}
-
-function updateScore(){
-  document.querySelector('.js-score').innerHTML=` Wins: ${score.wins} , Loses: ${score.loses} , Ties: ${score.ties}`;
-}
-
-function pickComputerMove(){ //WHAT COMPUTER CHOOSES
-  
-  let compMove= ''; 
-  const randomNum = Math.random();  
-  
-  if(randomNum >= 0 && randomNum < 1/3){
-    compMove= 'Rock';
-  }else if(randomNum >= 1/3 && randomNum < 2/3){
-    compMove= 'Paper';
-  }else{ 
-    compMove= 'Scissors'; 
-  }
-  
-  return compMove;  
->>>>>>> d4ecf64b1014994c4d4417992d30b644a0e836ce
 }
--- Simple-Calculator/script.js
+++ Simple-Calculator/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let calculation = localStorage.getItem('calculation') || ''; //get it out of the local storage
 
 function updateCalculation(num){
@@ -9,16 +8,4 @@ function updateCalculation(num){
 
 function displayOnscreen(){
   document.querySelector('.js-display').innerHTML= calculation;
-=======
-let calculation = localStorage.getItem('calculation') || ''; //get it out of the local storage
-
-function updateCalculation(num){
-  calculation+=num;
-  displayOnscreen();
-  localStorage.setItem('calculation',calculation); //stores the value of calculation variable locally
-}
-
-function displayOnscreen(){
-  document.querySelector('.js-display').innerHTML= calculation;
->>>>>>> d4ecf64b1014994c4d4417992d30b644a0e836ce
 }
